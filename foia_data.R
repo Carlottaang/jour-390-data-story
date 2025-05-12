fe1bdbc0d194500bd68c4a6cee54050a5c94f4b3
Carlottaang <carlottaangiolillo2027@u.northwestern.edu>
2025-05-12 09:59:22 -0500

    exploring data

--- foia_data.R
+++ foia_data.R
@@ -13,8 +13,34 @@ foia_data |>
   skimr::skim_without_charts() |> 
   view()
 
+# breakdown of participants in diversion programs 
 foia_data |> 
-  group_by(diversion_program) |> 
+  group_by(diversion_program, race, gender) |> 
   count(offense_category) |> 
   arrange(desc(n)) |> 
   view()
+
+# female participants in programs 
+foia_data |> 
+  filter(gender == "Female") |> 
+  count(diversion_program) |> 
+  arrange(desc(n)) |> 
+  view()
+
+# male participants in programs
+foia_data |> 
+  filter(gender == "Male") |> 
+  count(diversion_program) |> 
+  arrange(desc(n)) |> 
+  view()
+
+
+
+
+
+  
+
+  
+  
+  
+  
