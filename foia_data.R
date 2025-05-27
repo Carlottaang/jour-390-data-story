8fe6f0ef3f4d0566d0cbe6a2787bca4e19437116
Carlottaang <carlottaangiolillo2027@u.northwestern.edu>
2025-05-27 00:48:23 -0500

    foia data processing

--- foia_data.R
+++ foia_data.R
@@ -12,11 +12,45 @@ foia_data <- read_csv(here("data/Angiolillo__Carlotta_responsive_docs (3).csv"))
 foia_data_2 <- foia_data |> 
   mutate(
     referral_date = as.Date(referral_date, format = "%m/%d/%Y"),
-    referral_year = format(referral_date, "%Y")
+    referral_year = format(referral_date, "%Y"),
+    diversion_closed_date = as.Date(diversion_closed_date, format = "%m/%d/%Y"),
+    diversion_closed_year = format(diversion_closed_date, "%Y")
+  ) |> 
+  # removing programs that no longer exist 
+  filter(
+    !diversion_program == "DS" & !diversion_program == "ARI"
+  ) |> 
+  # new variable - pre plea vs. post plea programs
+  mutate(
+    plea = case_when(
+      diversion_program %in% c("BR9", "DDPP", "RJCC", "SEED")  ~ "pre",
+      diversion_program %in% c("ACT", "DC", "MHC", "VC")  ~ "post"
+    ) 
+  ) |> 
+  # finding number of days between when defendant was referred to a diversion program and when they graduated/failed 
+  mutate(
+    days_between = as.numeric(
+      as.Date(diversion_closed_date) - as.Date(referral_date)
+    )
+  ) |> 
+  # finding years, rounded to two decimal places 
+  mutate(
+    years = round(days_between / 365, 2) 
+  ) |> 
+  # categorizing how long people have spent in programs
+  mutate(
+    one_year = case_when(
+      # less than one year 
+      years <= 0.99 ~ "less",
+      # one year 
+      years == 1.00 ~ "one",
+      # more than one year 
+      years >= 1.01 ~ "more"
+    )
   ) 
 
 
-# exploring data 
+# data exploration
 foia_data |> 
   skimr::skim_without_charts() |> 
   view()
